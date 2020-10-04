5ab389499b9a013c48557286fc4401fae7e68250
Kim Haewoon <71933717+KimHaewoon@users.noreply.github.com>
2020-10-04 16:33:51 +0500

    Add files via upload

---
+++ MEMOIZED.cpp
@@ -0,0 +1,63 @@
+
+/* *************************** CUT-ROD RECURSIVE ALGORITHM IMPLEMENTATION *************************** */
+
+#include<iostream>
+using namespace std;
+
+int COUNT = 0;
+
+
+int memoized_Cut_Rod_Aux(int p[], int n, int r[])
+{
+	int q = 0;
+	int min_Value = INT_MIN;
+
+	if (r[n] >= 0)
+	{
+		return r[n];
+	}
+	if (n == 0)
+	{
+		q = 0;
+	}
+	else
+	{
+		COUNT++;
+
+		for (int i = 0; i < n; ++i)
+		{
+			int q = p[i] + memoized_Cut_Rod_Aux(p, n - i - 1, r);
+
+			if (q < min_Value)
+			{
+				min_Value = q;
+			}
+			r[i] = min_Value;
+		}
+
+	}
+	return min_Value;
+
+}
+
+
+
+
+
+int main()
+{
+	//int length[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int price[] = { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+
+	int num;
+
+	cout << "Enter value of num: ";
+	cin >> num;
+
+	cout << endl;
+	cout << "Profit you may get is: " << memorized_Cut_Rod(price, num) << endl;
+	cout << "Number of function calls are: " << COUNT << endl << endl;
+
+	system("pause");
+	return 0;
+}
